fff77dee376611446e612ece7b69d8d20d3c2489
Gliga Dumitru <gliga_dumitru@yahoo.com>
2020-02-11 12:19:22 +0200

    Updates

---
+++ precache-manifest.7d519baf366fbfbfba9a76bda947be36.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23a9c66abbe7715365621cab768de24e",
+    "url": "/shop-clothing/index.html"
+  },
+  {
+    "revision": "06060519bd0a0fb4890a",
+    "url": "/shop-clothing/static/css/main.940ffe07.chunk.css"
+  },
+  {
+    "revision": "6c7d820867d82bb25209",
+    "url": "/shop-clothing/static/js/2.0d9905ad.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/shop-clothing/static/js/2.0d9905ad.chunk.js.LICENSE"
+  },
+  {
+    "revision": "06060519bd0a0fb4890a",
+    "url": "/shop-clothing/static/js/main.779623bf.chunk.js"
+  },
+  {
+    "revision": "976c0ceb8d3cc49a23cf",
+    "url": "/shop-clothing/static/js/runtime-main.21bacdc3.js"
+  }
+]);
